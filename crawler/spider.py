70b29e46f9174f43811dfac0eb14dd46409a2ee8
JiaxinDu <jd688@njit.edu>
2020-03-24 10:59:45 -0400

    revert to djx's version

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,112 +1,61 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-r"""
-Life's pathetic, have fun ("▔□▔)/hi~♡ Nasy.
-
-Excited without bugs::
-
-    |             *         *
-    |                  .                .
-    |           .
-    |     *                      ,
-    |                   .
-    |
-    |                               *
-    |          |\___/|
-    |          )    -(             .              ·
-    |         =\ -   /=
-    |           )===(       *
-    |          /   - \
-    |          |-    |
-    |         /   -   \     0.|.0
-    |  NASY___\__( (__/_____(\=/)__+1s____________
-    |  ______|____) )______|______|______|______|_
-    |  ___|______( (____|______|______|______|____
-    |  ______|____\_|______|______|______|______|_
-    |  ___|______|______|______|______|______|____
-    |  ______|______|______|______|______|______|_
-    |  ___|______|______|______|______|______|____
-
-author   : Nasy https://nasy.moe
-date     : Mar 20, 2020
-email    : Nasy <nasyxx+python@gmail.com>
-filename : spider.py
-project  : crawler
-license  : GPL-3.0+
-
-At pick'd leisure
-  Which shall be shortly, single I'll resolve you,
-Which to you shall seem probable, of every
-  These happen'd accidents
-                          -- The Tempest
-"""
-
-# Standard Library
-from functools import reduce
-from multiprocessing.dummy import Pool
-from operator import or_
-from pathlib import Path
-
-# Others
-from httpx import Client
-from lxml.html import fromstring
+import requests
+from bs4 import BeautifulSoup
 from tqdm import tqdm
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
 
-# Types
-from typing import Set
+def get_all_topics():
+    url_base = "https://gistbok.ucgis.org/all-topics?term_node_tid_depth=All&page="
+    url_list = [url_base + str(i) for i in range(0, 30)]
 
-DATA_PATH = Path("./data")
-URL_BASE = "https://gistbok.ucgis.org"
-PAGES = 30
-
-
-def fetch(client: Client, url: str) -> str:
-    """Fetch url and the response text back."""
-    return client.get(url).text
-
-
-def parse_links(text: str) -> Set[str]:
-    """Get links from topics' html."""
-    return set(map(lambda href: href, fromstring(text).xpath("//h5/a/@href")))
-
-
-def fetch_all_topics() -> Set[str]:
-    with Client() as client:
-        with Pool() as pool:
-            return reduce(
-                or_,
-                pool.map(
-                    lambda page: parse_links(
-                        fetch(
-                            client,
-                            f"{URL_BASE}/all-topics?"
-                            f"term_node_tid_depth=All&page={page}",
-                        )
-                    ),
-                    range(PAGES),
-                ),
-            )
+    topic_set = set()
+    for i, url in tqdm(enumerate(url_list)):
+        r = requests.get(url)
+        to_file_path = "./data/topics_page_" + str(i) + ".html"
+        with open(to_file_path, "w") as f:
+            f.write(r.text)
+        # read a html file and extract what you need using BeautifulSoup
+        with open(to_file_path, "r") as f:
+            html = f.read()
+        soup = BeautifulSoup(html, "html.parser")
+        hrefs = soup.find_all('a')
+        for href in hrefs:
+            href_text = href.get('href')
+            if href_text.startswith('/bok-topics'):
+                topic_set.add(href_text.replace("/bok-topics/", ""))
+        return topic_set
 
 
-def fetch_topic(client: Client, url: str, pbar: tqdm = None) -> None:
-    """Fetch and save each topic original html."""
-    path = DATA_PATH / "htmls" / Path(url).relative_to("/")
-    path.parent.mkdir(parents=True, exist_ok=True)
-    with path.with_suffix(".html").open("w") as f:
-        f.write(client.get(f"{URL_BASE}{url}").text)
-    if pbar:
-        pbar.update()
-
-
-def main() -> None:
-    """Main function."""
-    with Client() as client:
-        with Pool(3) as pool:
-            topics = fetch_all_topics()
-            pbar = tqdm(total=len(topics))
-            pool.map(lambda url: fetch_topic(client, url, pbar), topics)
+def save_urls(topic_set):
+    for topic in tqdm(topic_set):
+        r = requests.get("https://gistbok.ucgis.org/bok-topics/" + topic)
+        to_file_path = "./data/gisbok_topic_" + topic + ".html"
+        with open(to_file_path, "w", encoding='utf-8') as f:
+            f.write(r.text)
 
 
 if __name__ == "__main__":
-    main()
+    # save the web page to a html file
+    # topic_set = get_all_topics()
+    onlyfiles = [f for f in listdir("./data") if isfile(join("./data", f))]
+    links={}
+    # read a html file and extract what you need using BeautifulSoup
+    for file in onlyfiles:
+        if file.startswith("gisbok_topic"):
+            with open("./data/"+file, "r", encoding='utf-8') as f:
+                html = f.read()
+            soup = BeautifulSoup(html, "html.parser")
+            learning_objectives = soup.find(class_="field-name-field-learning-objectives").find_all('li')
+            for lo in learning_objectives:
+                print(lo.text)
+            title = soup.find(id='page-title').string
+            related_topics = soup.find(id="related-topics")
+            if related_topics == None:
+                continue
+            rs = []
+            for r in related_topics.find_all('a'):
+                rs.append(r.string)
+            links[title]=rs
+    df = pd.DataFrame.from_dict(links)
+    df.to_csv("./data/links.csv")
