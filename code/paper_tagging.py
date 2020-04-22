17e9e8fb38a1cc7a717c51980a51dbdec388e7d7
JiaxinDu <jd688@njit.edu>
2020-04-22 15:59:28 -0400

    bag of words

--- code/paper_tagging.py
+++ code/paper_tagging.py
@@ -47,10 +47,10 @@ def bow_papers(x1,x2,ps, tfidf_vectorizer):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("echo")
-    args = parser.parse_args()
-    print(args.echo)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("echo")
+    # args = parser.parse_args()
+    # print(args.echo)
     gisbokraw = pd.read_csv('adjusted.csv')
     gisbok = gisbokraw.fillna(axis=1, method='ffill')
     topics_gisbok = gisbok["topic"].to_list()
@@ -66,25 +66,44 @@ if __name__ == "__main__":
 
     groupTopic_gisbok = gisbok.groupby("topic")
     embed_topic_gisbok = bow_topics(groupTopic_gisbok, tfidf_vectorizer)
-    ebk = pd.read_csv('en.csv', quotechar='@')
-    ebk['embedding'] = ebk.apply(lambda x: bow_papers(x['abstract'], x['PaperTitle'], ps, tfidf_vectorizer), axis=1)
+    # ebk = pd.read_csv('en.csv', quotechar='@')
+    # ebk['embedding'] = ebk.apply(lambda x: bow_papers(x['abstract'], x['PaperTitle'], ps, tfidf_vectorizer), axis=1)
+    #
+    # embedded_gisbok = scipy.sparse.vstack(embed_topic_gisbok["embedding"].to_list())
+    # embedded_ebk = scipy.sparse.vstack(ebk["embedding"].to_list())
+    # similarity_score_matrix = embedded_gisbok.dot(embedded_ebk.T)
 
-    embedded_gisbok = scipy.sparse.vstack(embed_topic_gisbok["embedding"].to_list())
-    embedded_ebk = scipy.sparse.vstack(ebk["embedding"].to_list())
-    similarity_score_matrix = embedded_gisbok.dot(embedded_ebk.T)
-    np.save('similar.npy',similarity_score_matrix.toarray())
-    index_sort = similarity_score_matrix.toarray().argsort(axis=-1)
+    similarity_score_matrix = np.load('similar.npy')
+    index_sort = similarity_score_matrix.argsort(axis=-1)
+    ebk=pd.read_pickle('embedding.pickle')
+    topic_paper_list = []
     for i in range(similarity_score_matrix.shape[0]):
-        if np.sum(similarity_score_matrix[i]) == 0:
-            print(i)
-    ebk.to_pickle('embedding.pickle')
+        print("topic:")
+        print(embed_topic_gisbok["topic"].iloc[i])
+        print("title:")
+        paper_list_40 = []
+        paper_list_0 = []
+        for ii in range(similarity_score_matrix.shape[1]):
+            if (similarity_score_matrix[i][index_sort[0][0-ii]]) > 0.4:
+                print(ebk["PaperTitle"].iloc[0-ii])
+                paper_list_40.append(ebk["PaperId"].iloc[0-ii])
+            elif(similarity_score_matrix[i][index_sort[0][0-ii]]) > 0:
+                print(ebk["PaperTitle"].iloc[0-ii])
+                paper_list_0.append(ebk["PaperId"].iloc[0-ii])
+        topic_paper_list.append(
+            {"topic":embed_topic_gisbok["topic"].iloc[i],"PaperIdList40":paper_list_40,"PaperIdList0":paper_list_0}
+        )
+    pd.DataFrame(topic_paper_list).to_pickle("topic_papers.pickle")
+
+
+
     index_very_similar = np.argwhere(
         similarity_score_matrix > 0.4)  # similarity_score_matrix is a symetic square matrix
     print(len(index_very_similar))
     set(index_very_similar[:, 0])
-    pairs = set()
+    pairs = []
     for i in index_very_similar:
-        pairs.add(embed_topic_gisbok["topic"].iloc[i[0]] + " ; " +ebk["PaperId"].iloc[i[1]])
+        pairs.append(embed_topic_gisbok["topic"].iloc[i[0]] + "," +str(ebk["PaperId"].iloc[i[1]]))
     print("\n".join(pairs))
 
     print("topic no match in gisbok:")
