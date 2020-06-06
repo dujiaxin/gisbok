84a5873c82fa8ba763fca8a2c69216d71cfea375
dujiaxin <dujiaxin@users.noreply.github.com>
2020-06-06 07:34:12 -0700

    Update paper_tagging.py

--- code/paper_tagging.py
+++ code/paper_tagging.py
@@ -5,8 +5,7 @@ import numpy as np
 from nltk.stem.porter import PorterStemmer
 from nltk import word_tokenize
 import scipy
-import argparse
-import multiprocessing
+from sklearn.preprocessing import normalize
 from gensim.models import KeyedVectors
 from gensim.utils import tokenize
 
@@ -18,6 +17,7 @@ def get_stopwords():
     all_stopwords = default_stopwords | custom_stopwords
     return all_stopwords
 
+stopwords=get_stopwords()
 
 def stem_lines(line, ps=PorterStemmer()):
     if len(line)<3:
@@ -25,7 +25,7 @@ def stem_lines(line, ps=PorterStemmer()):
         print(line)
         return 'null'
     tokens = word_tokenize(line)
-    tokens = [word for word in tokens if len(word) > 1]
+    tokens = [word for word in tokens if len(word) > 1 and (word not in stopwords)]
     stemmed_tokens = [ps.stem(i) for i in tokens]
     return ' '.join(stemmed_tokens)
 
@@ -54,11 +54,8 @@ def bow_topics(x, tfidf_vectorizer):
     return pd.DataFrame(embeddedTopic)
 
 
-def bow_papers(x1,x2,tfidf_vectorizer):
-    stemmed = stem_lines(x1+x2)
-    embedded_gisbok = tfidf_vectorizer.transform(stemmed)
-    return embedded_gisbok
-
+# def weight_normalize(array):
+#     return 0.001*normalize(array)
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
@@ -79,39 +76,38 @@ if __name__ == "__main__":
     # stems_gisbok = [stem_lines(i, ps) for i in topics_gisbok]
     stems_lo = [stem_lines(i, ps) for i in all_lo]
 
-    tfidf_vectorizer = TfidfVectorizer(stop_words=get_stopwords())
-    tfidf_vectorizer.fit(stems_lo)
+    tfidf_vectorizer = TfidfVectorizer()
+    embed_ontology_tfidf = tfidf_vectorizer.fit_transform(stems_lo)
 
     groupTopic_gisbok = gisbok.groupby("topic")
     # with Pool(cores) as p:
     #     sentences = p.map(stem_lines, list_ab)
     embed_topic_gisbok = bow_topics(groupTopic_gisbok, tfidf_vectorizer)
-    embed_ontology_tfidf = tfidf_vectorizer.transform(stems_lo)
+
     print("embed_ontology_tfidf.shape")
     print(embed_ontology_tfidf.shape)
     wv = KeyedVectors.load("fastText_model_real")
     embed_ontology_fasttext = np.array([wv_gisbok(i,wv) for i in all_lo])
-    embed_ontology_scibert = np.load("gisbok_scibert_embedding.npy")
+    embed_ontology_scibert = np.load("gisbok_bert_embedding.npy")
     print(embed_ontology_fasttext.shape)
     print(embed_ontology_scibert.shape)
     embed_ontology = np.hstack((embed_ontology_tfidf.toarray(),
-                               embed_ontology_fasttext,
-                               embed_ontology_scibert))
+                               normalize(embed_ontology_fasttext),
+                               normalize(embed_ontology_scibert)))
     print("embed_ontology.shape")
     print(embed_ontology.shape)
     ebk = pd.read_csv('en.csv', quotechar='@')
     ebk['stems'] = ebk.apply(lambda x: stem_lines(x['abstract'] + x['PaperTitle']), axis=1)
 
-    word_vectors = KeyedVectors.load("fastText_model_real")
-
     embedded_ebk_tfidf = (tfidf_vectorizer.transform(ebk['stems'].to_list())).toarray()
     embedded_ebk_fasttext = pd.read_pickle("fasttext_real_embedding.pickle")
     embedded_ebk_scibert = pd.read_pickle("scibert_embedding.pickle")
+    embedded_ebk_scibert['scibert_embedding'] = np.load('titleAbs_bert_embedding.npy').tolist()
     print("embedded_ebk_tfidf.shape")
     print(embedded_ebk_tfidf.shape)
     embedded_ebk = np.hstack((embedded_ebk_tfidf,
-                            np.array(embedded_ebk_fasttext["fasttext_embedding"].to_list()),
-                             np.squeeze(np.array(embedded_ebk_scibert["scibert_embedding"].to_list()))
+                            normalize(np.array(embedded_ebk_fasttext["fasttext_embedding"].to_list())),
+                             normalize(np.squeeze(np.array(embedded_ebk_scibert["scibert_embedding"].to_list())))
                              ))
     print("embedded_ebk.shape")
     print(embedded_ebk.shape)
@@ -124,26 +120,17 @@ if __name__ == "__main__":
 
     topic_paper_list = []
     for i in range(s.shape[0]):
-        print("all_lo:")
-        print(all_lo[i])
-        print("title:")
-        paper_list_400 = []
-        paper_list_300 = []
-        for ii in range(s.shape[1]):
-            if s[i][index_sort[i][-1 - ii]] > 400:
-                print(ebk["PaperTitle"].iloc[index_sort[i][-1 - ii]])
-                paper_list_400.append(ebk["PaperId"].iloc[index_sort[i][-1 - ii]])
-            elif s[i][index_sort[i][-1 - ii]] > 300:
+        paper_list_0 = []
+        for ii in range(0,10000):
+            if s[i][index_sort[i][-1 - ii]] > 0:
                 try:
-                    paper_list_300.append(ebk["PaperId"].iloc[index_sort[i][-1 - ii]])
+                    paper_list_0.append(ebk["PaperId"].iloc[index_sort[i][-1 - ii]])
                 except:
                     pass
             else:
                 break
-
         topic_paper_list.append(
-            {"all_lo":all_lo[i],"PaperIdList400":paper_list_400,"PaperIdList300":paper_list_300}
+            {"topic":all_lo[i].lower(),"PaperIdList0":paper_list_0}
         )
-    pd.DataFrame(topic_paper_list).to_pickle("topic_papers.pickle")
-
+    pd.DataFrame(topic_paper_list).to_pickle("nodes_papers.pickle")
 
